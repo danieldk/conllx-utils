a162ca08a0537f6acecd263119f83c426226a6a1
Daniël de Kok <me@danieldk.eu>
2018-02-15 14:23:13 +0100

    Support shuffling with a fixed seed.

--- src/bin/conllx-shuffle.rs
+++ src/bin/conllx-shuffle.rs
@@ -10,7 +10,7 @@ use std::io::BufWriter;
 use conllx::WriteSentence;
 use conllx_utils::or_exit;
 use getopts::Options;
-use rand::Rng;
+use rand::{Rng, SeedableRng, XorShiftRng};
 use stdinout::{Input, OrExit, Output};
 
 fn print_usage(program: &str, opts: Options) {
@@ -24,6 +24,7 @@ fn main() {
 
     let mut opts = Options::new();
     opts.optflag("h", "help", "print this help menu");
+    opts.optopt("s", "seed", "RNG seed", "SEED");
     let matches = or_exit(opts.parse(&args[1..]));
 
     if matches.opt_present("h") {
@@ -36,7 +37,16 @@ fn main() {
         return;
     }
 
-    let mut rng = rand::weak_rng();
+    let seed = if let Some(seed_str) = matches.opt_str("s") {
+        let seed_val: u32 = seed_str
+            .parse()
+            .or_exit(format!("Cannot not parse '{}' as an integer", seed_str), 1);
+        [seed_val; 4]
+    } else {
+        rand::thread_rng().gen()
+    };
+
+    let mut rng = XorShiftRng::from_seed(seed);
 
     let input = Input::from(matches.free.get(0));
     let reader = conllx::Reader::new(or_exit(input.buf_read()));
@@ -48,6 +58,7 @@ fn main() {
         .into_iter()
         .map(|r| r.or_exit("Cannot read sentence", 1))
         .collect();
+
     rng.shuffle(&mut sents);
 
     for sent in sents {
