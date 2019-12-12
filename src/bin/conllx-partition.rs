ba382374f4175761423e6ed01c4f1116506107fd
Daniël de Kok <me@danieldk.eu>
2019-12-12 19:04:53 +0100

    Add the "-r" option for random partitioning

--- src/bin/conllx-partition.rs
+++ src/bin/conllx-partition.rs
@@ -1,54 +1,141 @@
-use std::env::args;
-use std::io::BufRead;
+use std::io::{self, BufRead};
 
-use conllx::{PartitioningWriter, WriteSentence};
+use clap::{App, AppSettings, Arg};
+use conllx::{PartitioningWriter, Sentence, WriteSentence};
 use conllx_utils::{open_writer, or_exit};
-use getopts::Options;
-use stdinout::Input;
+use rand::{Rng, StdRng};
+use std::process;
+use stdinout::{Input, OrExit};
 
-fn print_usage(program: &str, opts: Options) {
-    let brief = format!("Usage: {} N PREFIX SUFFIX [FILE]", program);
-    print!("{}", opts.usage(&brief));
+static DEFAULT_CLAP_SETTINGS: &[AppSettings] = &[
+    AppSettings::DontCollapseArgsInUsage,
+    AppSettings::UnifiedHelpMessage,
+];
+
+static INPUT: &str = "INPUT";
+static PARTS: &str = "PARTS";
+static PREFIX: &str = "PREFIX";
+static RANDOM: &str = "RANDOM";
+static SUFFIX: &str = "SUFFIX";
+
+struct RandomPartitioningWriter<R, W>
+where
+    R: Rng,
+    W: WriteSentence,
+{
+    rng: R,
+    writers: Vec<W>,
+}
+
+impl<R, W> RandomPartitioningWriter<R, W>
+where
+    R: Rng,
+    W: WriteSentence,
+{
+    pub fn new(rng: R, writers: Vec<W>) -> Self {
+        RandomPartitioningWriter { rng, writers }
+    }
+}
+
+impl<R, W> WriteSentence for RandomPartitioningWriter<R, W>
+where
+    R: Rng,
+    W: WriteSentence,
+{
+    fn write_sentence(&mut self, sentence: &Sentence) -> Result<(), io::Error> {
+        let fold = self.rng.gen_range(0, self.writers.len());
+        self.writers[fold].write_sentence(sentence)
+    }
+}
+
+pub struct PartitionApp {
+    input: Option<String>,
+    parts: usize,
+    prefix: String,
+    random: bool,
+    suffix: String,
+}
+
+impl PartitionApp {
+    fn new() -> Self {
+        let matches = App::new("conllx-partition")
+            .settings(DEFAULT_CLAP_SETTINGS)
+            .arg(Arg::with_name(PARTS).help("Output data").index(1))
+            .arg(
+                Arg::with_name(PREFIX)
+                    .help("Output data")
+                    .index(2)
+                    .required(true),
+            )
+            .arg(
+                Arg::with_name(SUFFIX)
+                    .help("Output data")
+                    .index(3)
+                    .required(true),
+            )
+            .arg(Arg::with_name(INPUT).help("Input data").index(4))
+            .arg(
+                Arg::with_name(RANDOM)
+                    .long("random")
+                    .short("r")
+                    .help("Write to random partitions"),
+            )
+            .get_matches();
+
+        let parts = matches
+            .value_of(PARTS)
+            .unwrap()
+            .parse()
+            .or_exit("Cannot parse number of parts", 1);
+        if parts == 0 {
+            eprintln!("Input should be split into at least one part");
+            process::exit(1);
+        }
+
+        let prefix = matches.value_of(PREFIX).unwrap().to_owned();
+        let suffix = matches.value_of(SUFFIX).unwrap().to_owned();
+        let input = matches.value_of(INPUT).map(ToOwned::to_owned);
+
+        PartitionApp {
+            input,
+            parts,
+            prefix,
+            random: matches.is_present(RANDOM),
+            suffix,
+        }
+    }
 }
 
 fn main() {
-    let args: Vec<String> = args().collect();
-    let program = args[0].clone();
+    let app = PartitionApp::new();
 
-    let mut opts = Options::new();
-    opts.optflag("h", "help", "print this help menu");
-    let matches = or_exit(opts.parse(&args[1..]));
-
-    if matches.opt_present("h") {
-        print_usage(&program, opts);
-        return;
-    }
-
-    if matches.free.len() < 3 || matches.free.len() > 4 {
-        print_usage(&program, opts);
-        return;
-    }
-
-    let n: usize = or_exit(matches.free[0].parse());
-    let prefix = &matches.free[1];
-    let suffix = &matches.free[2];
-
-    let input = Input::from(matches.free.get(3));
+    let input = Input::from(app.input.as_ref());
     let reader = conllx::Reader::new(or_exit(input.buf_read()));
 
-    let writers: Vec<_> = (0..n)
-        .map(|part| or_exit(open_writer(&format!("{}{}{}", prefix, part, suffix))))
+    let writers: Vec<_> = (0..app.parts)
+        .map(|part| {
+            or_exit(open_writer(&format!(
+                "{}{}{}",
+                app.prefix, part, app.suffix
+            )))
+        })
         .collect();
 
-    let writer = PartitioningWriter::new(writers);
+    let mut writer: Box<dyn WriteSentence> = if app.random {
+        Box::new(RandomPartitioningWriter::new(
+            StdRng::new().or_exit("Cannot initialize RNG from entropy", 1),
+            writers,
+        ))
+    } else {
+        Box::new(PartitioningWriter::new(writers))
+    };
 
-    copy_sents(reader, writer)
+    copy_sents(reader, &mut *writer)
 }
 
-fn copy_sents<R, W>(reader: conllx::Reader<R>, mut writer: W)
+fn copy_sents<R>(reader: conllx::Reader<R>, writer: &mut dyn WriteSentence)
 where
     R: BufRead,
-    W: WriteSentence,
 {
     for sentence in reader {
         let sentence = or_exit(sentence);
